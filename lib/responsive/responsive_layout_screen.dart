a7dd2fbb689b9fa990b20c7f2551783e905ef0dc
SatyamCTGS <satyam.pandey@ctgs.in>
2026-04-11 23:40:31 +0530

    First commit

---
+++ lib/responsive/responsive_layout_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:insta_clone_flutter/utils/dimensions.dart';
+
+class ResponsiveLayout extends StatelessWidget {
+  final Widget webScreenLayout;
+  final Widget mobileScreenLayout;
+  const ResponsiveLayout({
+    super.key,
+    required this.webScreenLayout,
+    required this.mobileScreenLayout,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (BuildContext context, Constraints) {
+        if (Constraints.maxWidth > webScreenSize) {
+          return webScreenLayout;
+        }
+        return mobileScreenLayout;
+      },
+    );
+  }
+}
