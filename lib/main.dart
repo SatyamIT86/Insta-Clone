21f343f65ac9a85a095539a35c2428b50659828f
SatyamCTGS <satyam.pandey@ctgs.in>
2026-04-12 13:02:09 +0530

    fixed .env

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:insta_clone_flutter/responsive/mobile_screen_layout.dart';
 import 'package:insta_clone_flutter/responsive/responsive_layout_screen.dart';
 import 'package:insta_clone_flutter/responsive/web_screen_layout.dart';
@@ -8,14 +9,15 @@ import 'package:insta_clone_flutter/utils/colors.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   if (kIsWeb) {
     await Firebase.initializeApp(
-      options: const FirebaseOptions(
-        apiKey: "AIzaSyB-fiRC4nH4idIqmnX9BpJhoyipT5n9l5s",
-        appId: "1:405469134268:web:3915745060424a5400bfb9",
-        messagingSenderId: "405469134268",
-        projectId: "insta-clone-9dbf3",
-        storageBucket: "insta-clone-9dbf3.firebasestorage.app",
+      options: FirebaseOptions(
+        apiKey: dotenv.env['API_KEY']!,
+        appId: dotenv.env['APP_ID']!,
+        messagingSenderId: dotenv.env['MESSAGING_SENDER_ID']!,
+        projectId: dotenv.env['PROJECT_ID']!,
+        storageBucket: dotenv.env['STORAGE_BUCKET']!,
       ),
     );
   } else {
